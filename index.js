823d293353df179f7e994c950b6666b50085c0bd
bishal <46195146+bishal-dhakal@users.noreply.github.com>
2023-03-28 20:36:31 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const fs = require("fs");
 
 //to create a folder
-//fs.mkdirSync("file");
+fs.mkdirSync("file");
 
 //to create a file
-//fs.writeFileSync('file/file.txt','a content to a file.');
+fs.writeFileSync('file/file.txt','a content to a file.');
 
-//fs.appendFileSync('file/file.txt',' more content to the file');
+fs.appendFileSync('file/file.txt',' more content to the file');
 
-//const data = fs.readFileSync('file/file.txt',"utf8");
+const data = fs.readFileSync('file/file.txt',"utf8");
 
-//console.log(data.toString())
-//console.log(data)
+console.log(data.toString())
+console.log(data)
 
-//fs.renameSync('file/file.txt','file/new.txt');
+fs.renameSync('file/file.txt','file/new.txt');
 
-//fs.unlinkSync('file/file.txt');
+fs.unlinkSync('file/file.txt');
 
 fs.rmdirSync("file");
+
+//while reading from a file / receiving packets over the internet.
+const buf_data = fs.readFileSync('read.txt');
+
+data = buf_data.toString();
+
+console.log(data);
+
+//rename a file
+
+fs.renameSync("read.txt","file.txt");
